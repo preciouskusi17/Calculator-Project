6aaa84b6538ea30c6705b3a8a3a55e7cfd609214
Precious Kusi <146889458+preciouskusi17@users.noreply.github.com>
2024-01-10 08:17:21 -0500

    Add functions to script.js
    Simple calculator has operators such as "+, -, *, /"
    Added functions add(), subtract(), multiply(),
    and divide() to perform basic math operations
    Added function operate() that takes an operator and two numbers
     and perform calculations on the two numbers

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a,b){
+    return a+b;
+};
+
+function subtract(a,b){
+    return a-b;
+};
+
+function multiply(a,b){
+    return a*b;
+};
+
+function divide(a,b){
+    return a/b;
+};
+
+const num1='';
+const operator='';
+const num2='';
+
+function operate (num1,operator,num2){
+    switch (operator){
+    case '+':
+        return add(num1,num2);
+    case '-':
+        return subtract(num1,num2);
+    case '*':
+        return multiply(num1,num2);
+    case '/':
+        return divide(num1,num2);
+}}
